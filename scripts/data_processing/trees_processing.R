ef4dd6e0b917cfbf15424d416233ea43ee828ff6
Don Radcliffe <donaldradcliffe91@gmail.com>
2021-04-06 11:49:30 -0700

    Formatted original data into easier to read treatment codes/labels and years since treatment. Made unique identifier for macroplot/treatment/code combinations. Created columns for number of burns/thins/pileburns.  Fixed date to yyyy/mm/dd.

--- scripts/data_processing/trees_processing.R
+++ scripts/data_processing/trees_processing.R
@@ -7,16 +7,14 @@
 ## See Github Repo https://github.com/don-radcliffe/nps_fx
 
 library(dplyr)
+library(stringr)
 library(tidyr)
 library(here)
 
-## Here skips the 'data' folder, wtf?? Try it if you're running on a different machine
-#here('data/')
-
-setwd('C:/ProgramR/nps_fx/data')
+here('data')
 trees_raw <- read.csv('data_raw/trees_raw.csv')
-#str(trees_raw)
 
+## Start processing the tree data
 trees <- trees_raw %>%
   ## Use mutate to rename columns to get rid of spaces and uppercase in column names
   ## with tolower() to get rid of uppercase in values
@@ -49,12 +47,91 @@ trees <- trees_raw %>%
   mutate(uv2 = tolower(UV2)) %>%
   mutate(uv3 = tolower(UV3)) %>%
   mutate(visited = tolower(Visited)) %>%
-  ## to remove the original columns; for some reason transmute() wouldn't work
-  select(c(date:visited))
+  ## need to remove the original columns; for some reason transmute() wouldn't work
+  select(c(date:visited)) %>%
+  ## below I run string splits on the monitoring_status column to isolate treatment code and year
+  ## by cutting off the first two and last two values in the string
+  ## a few codes cause problems: so I change them here so it gives me what I want later
+  ## 00pre, immediate pre-treatment, to -1pre-1
+  ## 00pro01, older pre-treatment, to -2post-2
+  ## XXpost, immediate post treatment with XX being treatment code, to XXpost00
+  mutate(monitoring_status = str_replace_all(monitoring_status, c('00pre' = '-1pre-1',
+                                                                  '00pro01' = '-2pre-2',
+                                                                  'post' = 'post00'))) %>%
+  ## substr() the monitoring status column to get treatment code and year
+  ## .after command places it right after monitoring status code
+  mutate(years_post = str_sub(monitoring_status, start = -2), .after = monitoring_status) %>%
+  mutate(treatment_code = str_sub(monitoring_status, end = 2), .after = monitoring_status) %>%
+  ## now copy the the treatment code column
+  ## so we can turn the codes into treatment names while keeping the treatment code column as a check
+  mutate(treatment = treatment_code, .after = years_post) %>%
+  ## now rename them, treatments in chronological order
+  mutate(treatment = str_replace_all(treatment, c('-2' = 'pretreatment_early',
+                                                  '-1' = 'pretreatment_current',
+                                                  '01' = 'burn',
+                                                  '02' = 'thin',
+                                                  '03' = 'pileburn',
+                                                  '04' = 'thin_burn',
+                                                  '05' = 'thin_burn_burn',
+                                                  '06' = 'thin_burn_burn_burn',
+                                                  '07' = 'thin_thin_burn',
+                                                  '08' = 'thin_thin_burn_burn',
+                                                  '09' = 'thin_thin_burn_burn_burn',
+                                                  '20' = 'pileburn_thin'))) %>%
+  ## let's also give numeric columns for numbers of the different treatments
+  ## there's probably a more elegant way to do this
+  ## burns
+  mutate(burns = treatment, .after = treatment) %>%
+  mutate(burns = str_replace_all(burns, c('pretreatment_early' = '0',
+                                          'pretreatment_current' = '0',
+                                          'burn' = '1',
+                                          'thin' = '0',
+                                          'pileburn' = '0',
+                                          'thin_burn' = '1',
+                                          'thin_burn_burn' = '2',
+                                          'thin_burn_burn_burn' = '3',
+                                          'thin_thin_burn' = '1',
+                                          'thin_thin_burn_burn' = '2',
+                                          'thin_thin_burn_burn_burn' = '3',
+                                          'pileburn_thin' = '0'))) %>%
+  ## thins
+  mutate(thins = treatment, .after = burns) %>%
+  mutate(thins = str_replace_all(thins, c('pretreatment_early' = '0',
+                                          'pretreatment_current' = '0',
+                                          'burn' = '0',
+                                          'thin' = '1',
+                                          'pileburn' = '0',
+                                          'thin_burn' = '1',
+                                          'thin_burn_burn' = '1',
+                                          'thin_burn_burn_burn' = '1',
+                                          'thin_thin_burn' = '2',
+                                          'thin_thin_burn_burn' = '2',
+                                          'thin_thin_burn_burn_burn' = '2',
+                                          'pileburn_thin' = '0'))) %>%
+  ## pileburns
+  mutate(pileburns = treatment, .after = thins) %>%
+  mutate(pileburns = str_replace_all(pileburns, c('pretreatment_early' = '0',
+                                          'pretreatment_current' = '0',
+                                          'burn' = '0',
+                                          'thin' = '0',
+                                          'pileburn' = '1',
+                                          'thin_burn' = '0',
+                                          'thin_burn_burn' = '0',
+                                          'thin_burn_burn_burn' = '0',
+                                          'thin_thin_burn' = '0',
+                                          'thin_thin_burn_burn' = '0',
+                                          'thin_thin_burn_burn_burn' = '0',
+                                          'pileburn_thin' = '1'))) %>%
+  ## now we need a unique identifier for plot by treatment by sample period so we can summarize data by plot
+  unite(macroplot_unique, c('macroplot', 'treatment_code', 'years_post'), sep = '_', remove = FALSE) %>%
+  ## now lets fix that date column
+  ## split the date from the time
+  ## NA argument gets rid of the time, which isn't useful
+  separate(date, c('date', NA), sep = ' ') %>%
+  ## format to a date
+  mutate(date = as.Date(date, '%m/%d/%Y')) %>%
+  ## format to iso 8601 for sorting
+  mutate(date = format(date, '%Y/%m/%d'))
 
-View(trees)           
-  
-  
-  
-  
+View(trees)
 
